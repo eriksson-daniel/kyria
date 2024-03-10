a84447b75229868487689f888b3ff5c651c8d971
Daniel Eriksson <eriksson.daniel@protonmail.com>
2024-03-10 12:20:05 +0100

    layout

--- layout/src/App.tsx
+++ layout/src/App.tsx
@@ -10,93 +10,124 @@ type Columns = [Column, Column, Column, Column, Column, Column, Column, Column, 
 type ThumbStack = [string, string, string, string, string, string, string]
 type ThumbStacks = [ThumbStack, ThumbStack]
 type Layout = {
-  columns: Columns
-  thumbStacks: ThumbStacks
+    columns: Columns
+    thumbStacks: ThumbStacks
 }
 
 const layouts: Layout[] = daniel.layers.map(layer => {
-  const columns: Columns = [[], [], [], [], [], [], [], [], [], [], [], []]
-  const thumbStacks = [[
-    mapKey(layer[40]),
-    mapKey(layer[41]),
-    mapKey(layer[42]),
-    mapKey(layer[43]),
-    mapKey(layer[44]),
+    const columns: Columns = [[], [], [], [], [], [], [], [], [], [], [], []]
+    const thumbStacks: ThumbStacks = [[
+        mapKey(layer[40]),
+        mapKey(layer[41]),
+        mapKey(layer[42]),
+        mapKey(layer[43]),
+        mapKey(layer[44]),
 
-    mapKey(layer[30]),
-    mapKey(layer[31]),
+        mapKey(layer[30]),
+        mapKey(layer[31]),
 
-  ], [
-    mapKey(layer[45]),
-    mapKey(layer[46]),
-    mapKey(layer[47]),
-    mapKey(layer[48]),
-    mapKey(layer[49]),
+    ], [
+        mapKey(layer[45]),
+        mapKey(layer[46]),
+        mapKey(layer[47]),
+        mapKey(layer[48]),
+        mapKey(layer[49]),
 
-    mapKey(layer[32]),
-    mapKey(layer[33]),
-  ]]
+        mapKey(layer[32]),
+        mapKey(layer[33]),
+    ]]
 
 
-  layer.forEach((key, index) => {
-    if (index >= 12 * 3 + 4) return
-    let adjustedIndex = index
-    if (adjustedIndex >= 30 && adjustedIndex < 34) return
-    if (adjustedIndex >= 34) adjustedIndex -= 4
+    layer.forEach((key, index) => {
+        if (index >= 12 * 3 + 4) return
+        let adjustedIndex = index
+        if (adjustedIndex >= 30 && adjustedIndex < 34) return
+        if (adjustedIndex >= 34) adjustedIndex -= 4
 
-    const colIndex = adjustedIndex % 12
-    const column = columns[colIndex]
+        const colIndex = adjustedIndex % 12
+        const column = columns[colIndex]
 
-    if (column !== undefined) {
-      column.push(mapKey(key))
-    }
+        if (column !== undefined) {
+            column.push(mapKey(key))
+        }
 
-  })
+    })
 
-  return { columns, thumbStacks }
+    return { columns, thumbStacks }
 })
 
 function App() {
 
-  return layouts.map((layout, index) => (
-    <div key={index}>
-      <Layout layout={layout} />
-    </div>
-  ))
+    return layouts.map((layout, index) => (
+        <div key={index}>
+            <Layout layout={layout} />
+        </div>
+    ))
 }
 
 const Layout = ({ layout }: { layout: Layout }) => {
-  const left = layout.columns.slice(0, 6)
-  const right = layout.columns.slice(6)
-  return (
-    <div className='board'>
-      <div className='left half'>
+    const leftMain = layout.columns.slice(0, 6)
+    const rightMain = layout.columns.slice(6)
+    const leftThumbStack = layout.thumbStacks[0]
+    const rightThumbStack = layout.thumbStacks[1]
 
-        {left.map((column, index) =>
-          <KeyColumn key={index} column={column} />)}
-      </div>
-      <div className='right half'>
+    return (
+        <div className='board'>
+            <div className='half'>
+                <div className='left main'>
+                    {leftMain.map((column, index) => <KeyColumn key={index} column={column} />)}
+                </div>
 
-        {right.map((column, index) =>
-          <KeyColumn key={index} column={column} />)}
-      </div>
+                <ThumbStack thumbStack={leftThumbStack} side='left' />
+            </div>
 
+            <div className='half'>
+                <div className='right main'>
+                    {rightMain.map((column, index) => <KeyColumn key={index} column={column} />)}
+                </div>
 
-
-    </div>
-  )
+                <ThumbStack thumbStack={rightThumbStack} side='right' />
+            </div>
+        </div>
+    )
 }
 
 const KeyColumn = ({ column }: { column: Column }) => {
-  return (
-    <div className="column">
-      {column.map((key, index) => (
-        <div key={index} className="key">
-          {key}
+    return (
+        <div className="column">
+            {column.map((key, index) => (
+                <div key={index} className="key">
+                    {key}
+                </div>
+            ))}
         </div>
-      ))}
-    </div>
-  )
+    )
+}
+
+type Side = 'left' | 'right'
+
+const ThumbStack = ({ thumbStack, side }: { thumbStack: ThumbStack, side: Side }) => {
+    return (
+        <div className={`${side} thumbStack`
+        }>
+            <div className={`toprow`}>
+                {thumbStack.slice(5).map((key, index) => (
+                    <div key={index} className="key">
+                        {key}
+                    </div>
+                ))}
+            </div>
+
+            <div className='bottomrow'>
+                {thumbStack.slice(0, 5).map((key, index) => (
+                    <div key={index} className="key">
+                        {key}
+                    </div>
+                ))}
+            </div>
+
+        </div>
+    )
 }
 
 export default App
